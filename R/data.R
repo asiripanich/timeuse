14bc7a4f610cc28621cfe36e5c80f5f52c30c1f9
Amarin Siripanich <17020181+asiripanich@users.noreply.github.com>
2021-10-12 16:20:58 +1100

    Update

--- R/data.R
+++ R/data.R
@@ -1,12 +1,13 @@
-#'  International Classification of Activities for Time Use Statistics 2016
+#' International Classification of Activities for Time Use Statistics 2016
 #'
 #' See \url{https://unstats.un.org/unsd/classifications/Family/Detail/2083}
 #'
-#' @format A [tibble] with `r nrow(icatus_2016)` rows and 10 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
 #' @source \url{https://unstats.un.org/unsd/demographic-social/time-use/icatus-2016/}
 "icatus_2016"
+
+#' Australia Time Use Acitivity Classification
+#'
+#' See \url{https://www.abs.gov.au/AUSSTATS/abs@.nsf/Latestproducts/863BE05F5DF81C08CA2573F500148972?opendocument}
+#'
+#' @source \url{https://www.abs.gov.au/AUSSTATS/subscriber.nsf/log?openagent&41500%20tus%202006%20curf%20data%20items.xls&4150.0&Data%20Cubes&87316ADE9E373C4CCA2573F500151342&0&2006&21.02.2008&Latest}
+"au_2006"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -10,3 +10,23 @@ icatus_2016 <-
 icatus_2016
 
 usethis::use_data(icatus_2016, overwrite = TRUE)
+
+au_2006 <-
+  readxl::read_xls(
+    system.file("extdata", "41500 tus 2006 curf data items.xls", package = "timeuse"),
+    sheet = "Activity",
+    range = "B48:B272",
+    skip = 47
+  ) %>%
+  tibble::as_tibble() %>%
+  janitor::clean_names() %>%
+  tidyr::separate(nature_of_activity, into = c("code", "description"), sep = "\\.") %>%
+  mutate(across(.fns = ~ trimws(.x))) %>%
+  mutate(
+    code_level_1 = substr(code, 1, 1),
+    code_level_2 = substr(code, 1, 2),
+    code_level_3 = substr(code, 1, 3)
+  ) %>%
+  select(starts_with("code_"), description)
+
+usethis::use_data(au_2006, overwrite = TRUE)
